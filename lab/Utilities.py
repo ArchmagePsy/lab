a9544dd84559edf6587a4c8b0c854423860f1a53
ArchmagePsy <SeaOfCodeSi@gmail.com>
2017-07-01 10:45:04 +0100

    added resource generator, next gonna write some tests for it

--- lab/Utilities.py
+++ lab/Utilities.py
@@ -78,3 +78,9 @@ class Selector:
         ret = self.result
         self.result = self.origin
         return ret
+
+    def find_resources(root = os.getcwd()):
+        if os.path.isdir(root):
+            return Resources.Folder(os.path.basename(root), [find_resources(root = i) for i in os.listdir(root)], root)
+        else:
+            return Resources.File(os.path.basename(root), root)
