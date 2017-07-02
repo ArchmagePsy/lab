3ade792852bc97f1a976dd88ebd15f917a65ec32
ArchmagePsy <SeaOfCodeSi@gmail.com>
2017-07-02 16:32:27 +0100

    fixed the structure and setup up scripts so that all I need to do is WRITE THE BLOODY TESTS

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
-setup(name = "lab",
+setup(
+    name = "lab",
     version = "0.0.1",
     url = "http://github.com/ArchmagePsy/lab",
     author = "ArchmagePsy",
     author_email = "SeaOfCodeSi@gmail.com",
     license = "GPL3",
     packages = ["lab"],
+    test_suite = "tests.suite",
     zip_safe = False
 )
---
+++ tests/testFinder.py
@@ -0,0 +1,12 @@
+from lab import Utilities
+from lab import Resources
+import unittest
+
+class FinderTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
