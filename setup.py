ce2a0e46a1c88cd99b2dd5bb3de2fdf3cdae7f48
ArchmagePsy <SeaOfCodeSi@gmail.com>
2017-07-20 16:10:39 +0100

    I don't care if this isn't how software versions work I've changed it to this so deal with it

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "Lab",
-    version = "0.0.1",
+    version = "0.5.0",
     url = "http://github.com/ArchmagePsy/lab",
     author = "ArchmagePsy",
     author_email = "SeaOfCodeSi@gmail.com",
